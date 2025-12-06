ae79e458ed4f6a31413c1b08876d990fff57b2ca
hassan95eb <19h.amini95@gmail.com>
2025-12-06 13:07:58 +0330

    complete home section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,13 @@
-import data from "./data.json";
-interface Project {
-    id: number;
-    title: string;
-    description: string;
-    isFeatured?: boolean;
-}
+import HomeSection from "@/components/HomeSection";
+import db from "./db.json";
 
-async function fetchPortfolioData() {
-    await new Promise((resolve) =>
-        setTimeout(resolve, 2000)
-    );
-
-    return data;
-}
 async function Home() {
-    const portfolioData = await fetchPortfolioData();
-    return (
-        <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 bg-background">
-            <div className="text-luxury-gold text-3xl">
-                Hello Project
-            </div>
-            {portfolioData.map((project: Project) => (
-                <article
-                    key={project.id}
-                    className="p-6 border rounded-lg shadow-md"
-                >
-                    <h2 className="text-2xl font-semibold mb-2">
-                        {project.title}
-                    </h2>
-                    <p className="text-gray-700">
-                        {project.description}
-                    </p>
-                </article>
-            ))}
-        </div>
-    );
+  return (
+    <div className="font-sans p-8 pb-20 gap-16 sm:p-20 bg-background">
+      <section id="#home" className="h-screen">
+        <HomeSection data={db.home} />
+      </section>
+    </div>
+  );
 }
 export default Home;
