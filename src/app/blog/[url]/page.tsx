25ea1665ad115a66b6a2ab0338dbba5ea2eacdc0
hassan95eb <19h.amini95@gmail.com>
2025-12-09 20:23:52 +0330

    debug

--- src/app/blog/[url]/page.tsx
+++ src/app/blog/[url]/page.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 
-export default async function page({ params }) {
+export default async function page({
+   params,
+}: {
+   params: Promise<{ url: string }>;
+}) {
    const { url } = await params;
 
    return <div>page{url}</div>;
